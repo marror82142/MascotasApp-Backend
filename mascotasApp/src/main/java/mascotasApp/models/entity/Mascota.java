380e36616a385d70b8b80f12366efdd0e3ecbd19
unknown <yeenpool@hotmail.com>
2024-03-18 22:38:59 -0500

    create

--- mascotasApp/src/main/java/mascotasApp/models/entity/Mascota.java
+++ mascotasApp/src/main/java/mascotasApp/models/entity/Mascota.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="contacto")
+@Table(name="mascota")
 public class Mascota implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
